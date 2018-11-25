2c8f823c25b4abeb006259878b68366168060f0e
Alexandra Gor <cousinzodin@gmail.com>
2018-11-25 15:01:00 +0200

    create modules

---
+++ js/utils.js
@@ -0,0 +1,13 @@
+const app = document.querySelector(`#main`);
+
+export const getElementFromTemplate = (template) => {
+    const wrapper = document.createElement(`div`);
+    wrapper.innerHTML = template.trim();
+    return wrapper;
+};
+
+
+export const changeScreen = (element) => {
+    app.innerHTML = ``;
+    app.appendChild(element);
+};
